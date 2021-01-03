0ed7b3cd1a6bbcef56983f83bc1a716f4890f5bb
Kevin Esler <kevin.esler@gmail.com>
2021-01-03 18:58:58 -0500

    Progress

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 module Main where
 
-import qualified Taxes as T
+import Taxes ( bracketWidth, FilingStatus (..), OrdinaryRate (..))
 
 main :: IO ()
 main = 
-  let v = T.bracketWidth T.HeadOfHousehold (T.OrdinaryRate 12)
+  let v = bracketWidth HeadOfHousehold (OrdinaryRate 12)
   in
     print v
