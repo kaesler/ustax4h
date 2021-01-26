5421e839603efa0adbbb811b0c04a14b06108a16
Kevin Esler <kevin.esler@gmail.com>
2021-01-25 22:26:39 -0500

    NonEmpty where possible

--- src/Taxes.hs
+++ src/Taxes.hs
@@ -22,6 +22,8 @@ where
 
 import Data.Coerce (coerce)
 import qualified Data.List as List
+import Data.List.NonEmpty as NonEmpty (fromList, toList, reverse, zip, head, last, tail, (!!))
+import Data.Map.NonEmpty as NEMap (NEMap, fromList, assocs, keys, lookup, elems)
 import qualified Data.Map.Strict as Map
 import Data.Maybe (fromJust)
 
@@ -57,9 +59,9 @@ type SSRelevantIncome = Double
 
 type CombinedIncome = Double
 
-type OrdinaryBracketStarts = Map.Map OrdinaryRate BracketStart
+-- type OrdinaryBracketStarts = Map.Map OrdinaryRate BracketStart
 
-type QualifiedBracketStarts = Map.Map QualifiedRate BracketStart
+-- type QualifiedBracketStarts = Map.Map QualifiedRate BracketStart
 
 type TaxableOrdinaryIncome = Double
 
@@ -72,47 +74,51 @@ nonNeg x
   | x < 0.0 = 0.0
   | otherwise = x
 
-ordinaryBracketStarts :: FilingStatus -> Map.Map OrdinaryRate BracketStart
+ordinaryBracketStarts :: FilingStatus -> NEMap OrdinaryRate BracketStart
 ordinaryBracketStarts Single =
-  Map.fromList
-    [ (OrdinaryRate 10, BracketStart 0),
-      (OrdinaryRate 12, BracketStart 9950),
-      (OrdinaryRate 22, BracketStart 40525),
-      (OrdinaryRate 24, BracketStart 86375),
-      (OrdinaryRate 32, BracketStart 164925),
-      (OrdinaryRate 35, BracketStart 209425),
-      (OrdinaryRate 37, BracketStart 523600)
-    ]
+  NEMap.fromList $
+    NonEmpty.fromList
+      [ (OrdinaryRate 10, BracketStart 0),
+        (OrdinaryRate 12, BracketStart 9950),
+        (OrdinaryRate 22, BracketStart 40525),
+        (OrdinaryRate 24, BracketStart 86375),
+        (OrdinaryRate 32, BracketStart 164925),
+        (OrdinaryRate 35, BracketStart 209425),
+        (OrdinaryRate 37, BracketStart 523600)
+      ]
 ordinaryBracketStarts HeadOfHousehold =
-  Map.fromList
-    [ (OrdinaryRate 10, BracketStart 0),
-      (OrdinaryRate 12, BracketStart 14200),
-      (OrdinaryRate 22, BracketStart 54200),
-      (OrdinaryRate 24, BracketStart 86350),
-      (OrdinaryRate 32, BracketStart 164900),
-      (OrdinaryRate 35, BracketStart 209400),
-      (OrdinaryRate 37, BracketStart 523600)
-    ]
+  NEMap.fromList $
+    NonEmpty.fromList
+      [ (OrdinaryRate 10, BracketStart 0),
+        (OrdinaryRate 12, BracketStart 14200),
+        (OrdinaryRate 22, BracketStart 54200),
+        (OrdinaryRate 24, BracketStart 86350),
+        (OrdinaryRate 32, BracketStart 164900),
+        (OrdinaryRate 35, BracketStart 209400),
+        (OrdinaryRate 37, BracketStart 523600)
+      ]
 
 bottomRateOnOrdinaryIncome :: FilingStatus -> OrdinaryRate
-bottomRateOnOrdinaryIncome fs = head $ Map.keys $ ordinaryBracketStarts fs
+bottomRateOnOrdinaryIncome fs = NonEmpty.head $ NEMap.keys $ ordinaryBracketStarts fs
 
 topRateOnOrdinaryIncome :: FilingStatus -> OrdinaryRate
-topRateOnOrdinaryIncome fs = last $ Map.keys $ ordinaryBracketStarts fs
+topRateOnOrdinaryIncome fs = NonEmpty.last $ NEMap.keys $ ordinaryBracketStarts fs
 
-qualifiedBracketStarts :: FilingStatus -> Map.Map QualifiedRate BracketStart
+qualifiedBracketStarts :: FilingStatus -> NEMap QualifiedRate BracketStart
 qualifiedBracketStarts Single =
-  Map.fromList
-    [ (QualifiedRate 0, BracketStart 0),
-      (QualifiedRate 15, BracketStart 40400),
-      (QualifiedRate 20, BracketStart 445850)
-    ]
+  NEMap.fromList $
+    NonEmpty.fromList
+      [ (QualifiedRate 0, BracketStart 0),
+        (QualifiedRate 15, BracketStart 40400),
+        (QualifiedRate 20, BracketStart 445850)
+      ]
 qualifiedBracketStarts HeadOfHousehold =
-  Map.fromList
-    [ (QualifiedRate 0, BracketStart 0),
-      (QualifiedRate 15, BracketStart 54100),
-      (QualifiedRate 20, BracketStart 473850)
-    ]
+  NEMap.fromList $
+    NonEmpty.fromList
+      [ (QualifiedRate 0, BracketStart 0),
+        (QualifiedRate 15, BracketStart 54100),
+        (QualifiedRate 20, BracketStart 473850)
+      ]
 
 {- ltcgTaxStart :: FilingStatus -> Int
 ltcgTaxStart filingStatus =
@@ -120,58 +126,59 @@ ltcgTaxStart filingStatus =
     brackets <- Map.lookup filingStatus qualifiedBracketStarts
  -}
 
-distributionPeriods :: Map.Map Age DistributionPeriod
+distributionPeriods :: NEMap Age DistributionPeriod
 distributionPeriods =
-  Map.fromList
-    [ (Age 70, 27.4),
-      (Age 71, 26.5),
-      (Age 72, 25.6),
-      (Age 73, 24.7),
-      (Age 74, 23.8),
-      (Age 75, 22.9),
-      (Age 76, 22.0),
-      (Age 77, 21.2),
-      (Age 78, 20.3),
-      (Age 79, 19.5),
-      (Age 80, 18.7),
-      (Age 81, 17.9),
-      (Age 82, 17.1),
-      (Age 83, 16.3),
-      (Age 84, 15.5),
-      (Age 85, 14.8),
-      (Age 86, 14.1),
-      (Age 87, 13.4),
-      (Age 88, 12.7),
-      (Age 89, 12.0),
-      (Age 90, 11.4),
-      (Age 91, 10.8),
-      (Age 92, 10.2),
-      (Age 93, 9.6),
-      (Age 94, 9.1),
-      (Age 95, 8.6),
-      (Age 96, 8.1),
-      (Age 97, 7.6),
-      (Age 98, 7.1),
-      (Age 99, 6.7),
-      (Age 100, 6.3),
-      (Age 101, 5.9),
-      (Age 102, 5.5),
-      (Age 103, 5.2),
-      (Age 104, 4.9),
-      (Age 105, 4.5),
-      (Age 106, 4.2),
-      (Age 107, 3.9),
-      (Age 108, 3.7),
-      (Age 109, 3.4),
-      (Age 110, 3.1),
-      (Age 111, 2.9),
-      (Age 112, 2.6),
-      (Age 113, 2.4),
-      (Age 114, 2.1)
-    ]
+  NEMap.fromList $
+    NonEmpty.fromList
+      [ (Age 70, 27.4),
+        (Age 71, 26.5),
+        (Age 72, 25.6),
+        (Age 73, 24.7),
+        (Age 74, 23.8),
+        (Age 75, 22.9),
+        (Age 76, 22.0),
+        (Age 77, 21.2),
+        (Age 78, 20.3),
+        (Age 79, 19.5),
+        (Age 80, 18.7),
+        (Age 81, 17.9),
+        (Age 82, 17.1),
+        (Age 83, 16.3),
+        (Age 84, 15.5),
+        (Age 85, 14.8),
+        (Age 86, 14.1),
+        (Age 87, 13.4),
+        (Age 88, 12.7),
+        (Age 89, 12.0),
+        (Age 90, 11.4),
+        (Age 91, 10.8),
+        (Age 92, 10.2),
+        (Age 93, 9.6),
+        (Age 94, 9.1),
+        (Age 95, 8.6),
+        (Age 96, 8.1),
+        (Age 97, 7.6),
+        (Age 98, 7.1),
+        (Age 99, 6.7),
+        (Age 100, 6.3),
+        (Age 101, 5.9),
+        (Age 102, 5.5),
+        (Age 103, 5.2),
+        (Age 104, 4.9),
+        (Age 105, 4.5),
+        (Age 106, 4.2),
+        (Age 107, 3.9),
+        (Age 108, 3.7),
+        (Age 109, 3.4),
+        (Age 110, 3.1),
+        (Age 111, 2.9),
+        (Age 112, 2.6),
+        (Age 113, 2.4),
+        (Age 114, 2.1)
+      ]
 
 rmdFractionForAge :: Age -> Double
-rmdFractionForAge age = 1.0 / fromJust (Map.lookup age distributionPeriods)
+rmdFractionForAge age = 1.0 / fromJust (NEMap.lookup age distributionPeriods)
 
 over65Increment :: Integer
 over65Increment = 1350
@@ -188,17 +195,17 @@ bracketWidth fs rate =
   fromJust
     ( do
         let brackets = ordinaryBracketStarts fs
-        let rates = Map.keys brackets
-        let pairs = zip rates (tail rates)
+        let rates = NEMap.keys brackets
+        let pairs = Prelude.zip (toList rates) (NonEmpty.tail rates)
         pair <- List.find (\p -> fst p == rate) pairs
         let successor = snd pair
-        rateStart <- Map.lookup rate brackets
-        successorStart <- Map.lookup successor brackets
+        rateStart <- NEMap.lookup rate brackets
+        successorStart <- NEMap.lookup successor brackets
         Just (coerce successorStart - coerce rateStart)
     )
 
 ltcgTaxStart :: FilingStatus -> Integer
-ltcgTaxStart fs = coerce (Map.elems (qualifiedBracketStarts fs) !! 1)
+ltcgTaxStart fs = coerce $ (NonEmpty.!!) (NEMap.elems (qualifiedBracketStarts fs)) 1
 
 taxableSocialSecurityAdjusted :: Year -> FilingStatus -> SocialSecurityBenefits -> SSRelevantIncome -> Double
 taxableSocialSecurityAdjusted year filingStatus ssBenefits relevantIncome =
@@ -232,8 +239,8 @@ taxableSocialSecurity filingStatus ssBenefits relevantIncome =
 
 applyOrdinaryIncomeBrackets :: FilingStatus -> TaxableOrdinaryIncome -> Double
 applyOrdinaryIncomeBrackets fs taxableOrdinaryincome =
-  let bracketsDescending = reverse (Map.assocs (ordinaryBracketStarts fs))
-   in snd (foldr func (taxableOrdinaryincome, 0.0) bracketsDescending)
+  let bracketsDescending = NonEmpty.reverse (NEMap.assocs (ordinaryBracketStarts fs))
+   in snd (List.foldr func (taxableOrdinaryincome, 0.0) bracketsDescending)
   where
     func :: (OrdinaryRate, BracketStart) -> (Double, Double) -> (Double, Double)
     func (rate, BracketStart start) (incomeYetToBeTaxed, taxSoFar) =
@@ -245,8 +252,8 @@ applyOrdinaryIncomeBrackets fs taxableOrdinaryincome =
 
 applyQualifiedBrackets :: FilingStatus -> TaxableOrdinaryIncome -> QualifiedInvestmentIncome -> Double
 applyQualifiedBrackets fs taxableOrdinaryIncome qualifiedInvestmentIncome =
-  let bracketsDescending = reverse (Map.assocs (qualifiedBracketStarts fs))
-   in third (foldr func (taxableOrdinaryIncome, qualifiedInvestmentIncome, 0.0) bracketsDescending)
+  let bracketsDescending = NonEmpty.reverse (NEMap.assocs (qualifiedBracketStarts fs))
+   in third (List.foldr func (taxableOrdinaryIncome, qualifiedInvestmentIncome, 0.0) bracketsDescending)
   where
     totalIncome = taxableOrdinaryIncome + qualifiedInvestmentIncome
     third :: (a, b, c) -> c
