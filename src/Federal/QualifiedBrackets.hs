1e654ee2ad06ec668aa7b174134c0e2a2c1f5898
Kevin Esler <kevin.esler@gmail.com>
2021-12-31 11:21:40 -0500

    QualifiedBrackets filled

--- src/Federal/QualifiedBrackets.hs
+++ src/Federal/QualifiedBrackets.hs
@@ -1,3 +1,17 @@
 module Federal.QualifiedBrackets
-
+  ( inflateThresholds,
+    fromPairs,
+  )
 where
+
+import qualified Brackets
+import Data.Coerce (coerce)
+import Federal.FederalTaxRate (FederalTaxRate, mkFederalTaxRate)
+
+newtype QualifiedBrackets = QualifiedBrackets (Brackets.Brackets FederalTaxRate)
+
+inflateThresholds :: Double -> QualifiedBrackets -> QualifiedBrackets
+inflateThresholds factor (QualifiedBrackets brackets) = coerce $ Brackets.inflateThresholds factor brackets
+
+fromPairs :: [(Double, Integer)] -> QualifiedBrackets
+fromPairs pairs = coerce $ Brackets.fromPairs pairs mkFederalTaxRate
