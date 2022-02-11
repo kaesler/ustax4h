cd9a2c8aac08968f908e021fbdca26088a1e4dba
Kevin Esler <kevin.esler@gmail.com>
2022-02-11 10:45:04 -0500

    Year2020

--- src/Federal/Yearly/Year2020.hs
+++ src/Federal/Yearly/Year2020.hs
@@ -26,31 +26,47 @@ values =
   YearlyValues
     { regime = Trump,
       year = 2020,
-      perPersonExemption = makeFromInt undefined ,
+      perPersonExemption = makeFromInt 0,
       unadjustedStandardDeduction =
         \case
-          HeadOfHousehold -> makeFromInt undefined
-          Single -> makeFromInt undefined,
-      adjustmentWhenOver65 = makeFromInt undefined,
-      adjustmentWhenOver65AndSingle = makeFromInt undefined,
+          HeadOfHousehold -> makeFromInt 18650
+          Single -> makeFromInt 12400,
+      adjustmentWhenOver65 = makeFromInt 1300,
+      adjustmentWhenOver65AndSingle = makeFromInt 350,
       ordinaryBrackets =
         \case
           HeadOfHousehold ->
             OB.fromRPairs
-              [ 
+              [ (0, 10),
+                (14100, 12),
+                (53700, 22),
+                (85500, 24),
+                (163300, 32),
+                (207350, 35),
+                (518400, 37)
               ]
           Single ->
             OB.fromRPairs
-              [ 
+              [ (0, 10),
+                (9875, 12),
+                (40125, 22),
+                (85525, 24),
+                (163300, 32),
+                (207350, 35),
+                (518400, 37)
               ],
       qualifiedBrackets =
         \case
           HeadOfHousehold ->
             QB.fromRPairs
-              [ 
+              [ (0, 0),
+                (53600, 15),
+                (469050, 20)
               ]
           Single ->
             QB.fromRPairs
-              [ 
+              [ (0, 0),
+                (40000, 15),
+                (442450, 20)
               ]
     }
